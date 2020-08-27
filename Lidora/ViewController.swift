b7550e1c294855ba4abf6138ea5ca8eb2934617b
Kerby Jean <Kurbs@Kerbys-MacBook-Pro.local>
2020-08-27 09:47:59 -0400

    first commit

---
+++ Lidora/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lidora
+//
+//  Created by Kerby Jean on 8/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
